15a1dfef6bdc7e5d1100aa4cbe456c0f1d5eba3e
Reshma-Unni <reshmammu456@gmail.com>
2025-11-23 21:20:04 +0530

    Implemented Home layout structure with sidebar, header, footer, and content in Home.jsx.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Outlet />
-
+      <div className="d-flex flex-grow-1">
+        <Sidebar />
+        <div className="flex-grow-1 p-3">
+          <Outlet />
+        </div>
+      </div>
       <Footer />
     </div>
   );

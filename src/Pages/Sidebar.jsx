e40bec9d721ddc6b497905efaee1815991d21382
Reshma-Unni <reshmammu456@gmail.com>
2025-11-23 21:24:42 +0530

    Initial commit: Added a simple sidebar layout

---
+++ src/Pages/Sidebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <div
+      className="bg-dark text-white p-3 min-vh-100"
+      style={{ width: "250px" }}
+    >
+      <h3>Products</h3>
+    </div>
+  );
+}

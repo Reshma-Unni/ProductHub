fb6ea522583405303dba24ef8d6cbaca0974accc
Reshma-Unni <reshmammu456@gmail.com>
2025-11-23 21:17:43 +0530

    Modified route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,23 @@ import {
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Products from "./Pages/Products";
+import Sidebar from "./Pages/Sidebar";
+import DeleteProduct from "./Pages/DeleteProduct";
+import View from "./Pages/View";
 
 function App() {
   // return <Login/>
   // return  <Footer/>
   // return <Header/>
+  // return <Sidebar />;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/products" element={<Products />} /> */}
-        {/* <Route path="/delete/:id" element={<DeleteProduct />} /> */}
+        <Route path="/delete/:id" element={<DeleteProduct />} />
+        <Route path="/product/:id" element={<View />} />
+
         <Route path="/product-hub/dashboard" element={<Home />}>
           <Route path="products" element={<Products />} />
         </Route>

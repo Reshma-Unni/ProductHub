a5a15417489ec4d90cd26d60ad7022a27192dfab
Reshma-Unni <reshmammu456@gmail.com>
2025-12-18 16:29:39 +0530

    Update route path

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Products from "./Pages/Products";
-import Sidebar from "./Pages/Sidebar";
-import DeleteProduct from "./Pages/DeleteProduct";
-import View from "./Pages/View";
 
-function App() {
-  // return <Login/>
-  // return  <Footer/>
-  // return <Header/>
-  // return <Sidebar />;
+import View from "./Pages/ViewProd";
+import Layout from "./Pages/Layout";
+import EditProduct from "./Pages/EditProd";
+// import AddProd from "./Pages/AddProd";
+
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/delete/:id" element={<DeleteProduct />} />
-        <Route path="/product/:id" element={<View />} />
 
-        <Route path="/product-hub/dashboard" element={<Home />}>
-          <Route path="products" element={<Products />} />
+        <Route element={<Layout />}>
+          {/* <Route path="/product/addprod" element={<AddProd />} /> */}
+          <Route path="/product/:id" element={<View />} />
+          <Route
+            path="/product-hub/dashboard/products"
+            element={<Products />}
+          />
+          <Route path="/edit-product/:id" element={<EditProduct />} />
         </Route>
       </Routes>
     </Router>
   );
 }
-
-export default App;

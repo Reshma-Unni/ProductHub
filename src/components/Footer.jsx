47a557972de7343f3bc1a524058cd9a297d66265
Reshma-Unni <reshmammu456@gmail.com>
2025-12-24 17:20:26 +0530

    Update footer with background image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white py-3 mt-4">
-      <div className="container d-flex justify-content-between align-items-center">
-        <h2 className="m-0">@2025ProductHub</h2>
+    <footer
+      className="text-white py-4 mt-auto"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url('/src/assets/Image-bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="container d-flex justify-content-center align-items-center">
+        <span className="fw-semibold fs-5">
+          © 2025 <span className="text-warning">ProductHub</span>.
+        </span>
       </div>
     </footer>
   );

0b21f77f11a03e8e735a4d00289ef4d7646d187a
Reshma-Unni <reshmammu456@gmail.com>
2025-12-27 10:49:33 +0530

    Add cart routes and wrap app with CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,34 @@ import Layout from "./pages/Layout";
 import EditProduct from "./pages/EditProd";
 import AddProduct from "./pages/AddProd";
 import UserProvider from "./context/UserProvider";
+import { CartProvider } from "./context/CartContext";
+import Cart from "./pages/Cart";
 
 export default function App() {
   return (
     <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
 
-          <Route element={<Layout />}>
-            <Route
-              path="/product-hub/dashboard/products/:id"
-              element={<View />}
-            />
-            <Route
-              path="/product-hub/dashboard/products"
-              element={<Products />}
-            />
-            <Route path="/edit-product/:id" element={<EditProduct />} />
-            <Route path="/addproduct" element={<AddProduct />} />
-          </Route>
-        </Routes>
-      </Router>
+            <Route element={<Layout />}>
+              <Route
+                path="/product-hub/dashboard/products/:id"
+                element={<View />}
+              />
+              <Route
+                path="/product-hub/dashboard/products"
+                element={<Products />}
+              />
+              <Route path="/edit-product/:id" element={<EditProduct />} />
+              <Route path="/addproduct" element={<AddProduct />} />
+              <Route path="/cart" element={<Cart />} />
+            </Route>
+          </Routes>
+        </Router>
+      </CartProvider>
     </UserProvider>
   );
 }
